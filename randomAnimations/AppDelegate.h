c070b74277de05a6343a6449d1cc47bccd72460e
Pol Quintana <polkintana@gmail.com>
2014-10-24 16:51:06 +0200

    Initial Commit

---
+++ randomAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  randomAnimations
+//
+//  Created by Pol Quintana on 24/10/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ randomAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  randomAnimations
+//
+//  Created by Pol Quintana on 24/10/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
